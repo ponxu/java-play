609d83c9e185bb2bf12fa7fa3aab422fe5c0be4c
ponxu <xwz689@gmail.com>
2015-08-14 18:37:12 +0800

    rename

--- src/main/java/com/ponxu/onepiece/Connection.java
+++ src/main/java/com/ponxu/onepiece/Connection.java
@@ -1,12 +1,10 @@
 package com.ponxu.onepiece;
 
-import java.nio.ByteBuffer;
-
 /**
  * Created by xwz on 15-8-13.
  */
 public interface Connection {
-    public void write(ByteBuffer data);
+    public void write(byte[] data);
 
     public void close();
 }
--- src/main/java/com/ponxu/test/io/TestOnepiece.java
+++ src/main/java/com/ponxu/test/io/TestOnepiece.java
@@ -1,9 +1,10 @@
 package com.ponxu.test.io;
 
-import com.ponxu.onepiece.nio.NIOServer;
 import com.ponxu.onepiece.Server;
+import com.ponxu.onepiece.nio.NIOServer;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * Created by xwz on 15-8-13.
@@ -12,5 +13,16 @@ public class TestOnepiece {
     public static void main(String[] args) throws IOException {
         Server s = new NIOServer(8080);
         s.start();
+        System.out.println("listen on 8080");
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String line = scanner.nextLine();
+            System.out.println(line);
+            if ("q".equalsIgnoreCase(line)) {
+                s.stop();
+                // break;
+            }
+        }
     }
 }
