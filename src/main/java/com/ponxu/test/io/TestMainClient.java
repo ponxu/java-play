8c4d015d613d0e8a7dbc3dc64a0e5e11e491f504
ponxu <xwz689@gmail.com>
2015-08-14 14:12:49 +0800

    test

---
+++ src/main/java/com/ponxu/test/io/TestMainClient.java
@@ -0,0 +1,20 @@
+package com.ponxu.test.io;
+
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by xwz on 15-8-14.
+ */
+public class TestMainClient {
+    public static void main(String[] args) throws Exception {
+        List<Socket> sockets = new ArrayList<>();
+        for (int i = 0; i < 200000; i++) {
+            Socket s = new Socket("127.0.0.1", 8080);
+            System.out.println(i + " " + s);
+            sockets.add(s);
+        }
+        Thread.currentThread().join();
+    }
+}
