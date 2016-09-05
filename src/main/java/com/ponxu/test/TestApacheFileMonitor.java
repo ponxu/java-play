f0953cd9e671e01f2eba6444b2921b379985f766
xuwenzhao <xuwenzhao@apusapps.com>
2016-09-05 21:04:42 +0800

    test

---
+++ src/main/java/com/ponxu/test/TestApacheFileMonitor.java
@@ -0,0 +1,36 @@
+package com.ponxu.test;
+
+import com.ponxu.utils.Utils;
+import org.apache.commons.io.filefilter.FileFilterUtils;
+import org.apache.commons.io.monitor.FileAlterationListenerAdaptor;
+import org.apache.commons.io.monitor.FileAlterationMonitor;
+import org.apache.commons.io.monitor.FileAlterationObserver;
+
+import java.io.File;
+
+/**
+ * Created by ponxu on 16-9-5.
+ */
+public class TestApacheFileMonitor {
+    public static void main(String[] args) throws Exception {
+        FileAlterationObserver observer = new FileAlterationObserver("/tmp/test", FileFilterUtils.suffixFileFilter(".txt"));
+        observer.addListener(new FileAlterationListenerAdaptor() {
+            @Override
+            public void onFileCreate(File file) {
+                System.out.println(file + " create");
+            }
+
+            @Override
+            public void onFileDelete(File file) {
+                System.out.println(file + " delete");
+            }
+        });
+
+
+        FileAlterationMonitor monitor = new FileAlterationMonitor(100);
+        monitor.addObserver(observer);
+        monitor.start();
+        Utils.sleep(600000);
+        monitor.stop();
+    }
+}
